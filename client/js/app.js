12bb87960945f3acac8cae0f1561cd80cb628492
Shun Git <kwokshungit@gmail.com>
2018-04-10 13:13:16 +0800

    display routes as polylines

--- client/js/app.js
+++ client/js/app.js
@@ -6,3 +6,32 @@ let map = L.map('map').setView([center.x, center.y], 12).setMaxBounds(BOUNDS);
 
 let basemap = L.tileLayer(MAP_TILES, {detectRetina: true, maxZoom: 18, minZoom: 12});
 basemap.addTo(map);
+
+let stops;
+let services = {}, serviceType = {};
+
+let routeLayers = {};
+let routeLayerGroup = L.layerGroup().addTo(map);
+
+fetch('../data/bus-stops.json')
+  .then((r) => r.json())
+  .then((s) => { stops = s; })
+
+fetch('../data/bus-services.json')
+  .then((r) => r.json())
+  .then((s) => { services = s.services; serviceType = s.types; serviceLines(); });
+
+function getService(bus) {
+  return fetch(`../data/bus-services/${bus}.json`).then((r) => r.json())
+}
+
+function serviceLines() {
+  Object.keys(serviceType).forEach((k) => {
+    routeLayers[k] = L.layerGroup().addTo(routeLayerGroup);
+  });
+  services.forEach((s) => getService(s.no).then((b) => {
+    if ( !b[1].route ) return;
+    let pts = b[1].route.map((p) => p.split(',').map(parseFloat));
+    L.polyline(pts).addTo(routeLayers[s.type]);
+  }));
+}

c1f271ba1bcec596258b1e371fc99e06a46e990f
Shun Git <kwokshungit@gmail.com>
2018-04-17 20:51:25 +0800

    move stuff into init

--- client/js/app.js
+++ client/js/app.js
@@ -1,28 +1,39 @@
 const MAP_TILES = 'https://maps-{s}.onemap.sg/v3/Night/{z}/{x}/{y}.png';
 const BOUNDS = [[1.56073, 104.11475], [1.16, 103.502]];
 
-let center = L.bounds(...BOUNDS).getCenter();
-let map = L.map('map').setView([center.x, center.y], 12).setMaxBounds(BOUNDS);
+let map, basemap;
+let busGroup, busStopGroup;
+let routeLayerGroup, routeLayers = {};
+let stops, services = {}, serviceType = {};
 
-let basemap = L.tileLayer(MAP_TILES, {detectRetina: true, maxZoom: 18, minZoom: 12});
-basemap.addTo(map);
+init();
 
-let stops;
-let services = {}, serviceType = {};
+function init() {
+  let center = L.bounds(...BOUNDS).getCenter();
+  map = L.map('map').setView([center.x, center.y], 12).setMaxBounds(BOUNDS);
 
-let busGroup = L.layerGroup().addTo(map);
-let busStopGroup = L.layerGroup().addTo(map);
+  basemap = L.tileLayer(MAP_TILES, {detectRetina: true, maxZoom: 18, minZoom: 12});
+  basemap.addTo(map);
 
-let routeLayers = {};
-let routeLayerGroup = L.layerGroup().addTo(map);
+  busGroup = L.layerGroup().addTo(map);
+  busStopGroup = L.layerGroup().addTo(map);
+  routeLayerGroup = L.layerGroup().addTo(map);
 
-fetch('../data/bus-stops.json')
-  .then((r) => r.json())
-  .then((s) => { stops = s; showStops(); })
+  fetch('../data/bus-stops.json')
+    .then((r) => r.json())
+    .then((s) => {
+      stops = s;
+      showStops();
+    });
 
-fetch('../data/bus-services.json')
-  .then((r) => r.json())
-  .then((s) => { services = s.services; serviceType = s.types; serviceLines(); });
+  fetch('../data/bus-services.json')
+    .then((r) => r.json())
+    .then((s) => {
+      services = s.services;
+      serviceType = s.types;
+      serviceLines();
+    });
+}
 
 function getService(bus) {
   return fetch(`../data/bus-services/${bus}.json`).then((r) => r.json())
@@ -43,6 +54,7 @@ function serviceLines() {
   Object.keys(serviceType).forEach((k) => {
     routeLayers[k] = L.layerGroup().addTo(routeLayerGroup);
   });
+
   services.forEach((s) => getService(s.no).then((b) => {
     if ( !b[1].route ) return;
     let pts = b[1].route.map((p) => p.split(',').map(parseFloat));
